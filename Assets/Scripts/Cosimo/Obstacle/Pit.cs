75f73dd5a6e0620b10b87326fbcfcee4f6499189
Merlin4854 <novateam987@gmail.com>
2026-04-08 17:23:44 +0200

    Try Fix SpawnPoint

--- Assets/Scripts/Cosimo/Obstacle/Pit.cs
+++ Assets/Scripts/Cosimo/Obstacle/Pit.cs
@@ -6,29 +6,57 @@ public class Pit : MonoBehaviour
     [SerializeField] private float delayBeforeRespawn = 1f;
     private bool _isRespawning = false;
 
+    private CompositeCollider2D _collider;
 
+    private void Awake()
+    {
+        _collider = GetComponent<CompositeCollider2D>();
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.TryGetComponent(out Player player))
+
+        if (_isRespawning || collision.GetComponent<Player>() == null) return;
+
+        Debug.DrawLine(collision.bounds.min, collision.bounds.max, Color.red);
+        if (IsFullyContained(_collider, collision))
         {
-           if(GetComponent<Collider2D>().bounds.Contains(collision.bounds.min)
-           && GetComponent<Collider2D>().bounds.Contains(collision.bounds.max))
-           {
-
-           }
-
-            ColliderDistance2D dist = Physics2D.Distance(GetComponent<Collider2D>(), collision);
-            dist.isOverlapped 
-
-
+            Debug.Log("Boh");
+            if (collision.TryGetComponent(out Player player))
+            {
+                Debug.Log("Bah");
+                StartCoroutine(RespawnCoroutine(player));
+            }
         }
-       
     }
 
 
+
+    private bool IsFullyContained(CompositeCollider2D container, Collider2D target)
+    {
+        Bounds containerBounds = container.bounds;
+        Bounds targetBounds = target.bounds;
+
+        Debug.Log(container.bounds);
+        Debug.Log(target.bounds);
+
+        Debug.Log("Palla");
+
+        Debug.Log($"{targetBounds.min.x >= containerBounds.min.x}" +
+        $"{targetBounds.max.x <= containerBounds.max.x}" +
+          $"{targetBounds.min.y >= containerBounds.min.y}" +
+            $"{targetBounds.max.y <= containerBounds.max.y}");
+
+        return targetBounds.min.x > containerBounds.min.x &&
+           targetBounds.max.x < containerBounds.max.x &&
+          targetBounds.min.y > containerBounds.min.y &&
+          targetBounds.max.y > containerBounds.max.y;
+
+    }
+
     private IEnumerator RespawnCoroutine(Player player)
     {
+        _isRespawning = true;
         yield return new WaitForSeconds(delayBeforeRespawn);
         player.Respawn();
         _isRespawning = false;
