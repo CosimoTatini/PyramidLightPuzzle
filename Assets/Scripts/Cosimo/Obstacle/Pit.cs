f09398be514e1cb850b62af5788054beeae9273b
Merlin4854 <novateam987@gmail.com>
2026-04-10 15:56:05 +0200

    Fixing Pit

--- Assets/Scripts/Cosimo/Obstacle/Pit.cs
+++ Assets/Scripts/Cosimo/Obstacle/Pit.cs
@@ -1,4 +1,7 @@
+using NUnit.Framework;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Pit : MonoBehaviour
@@ -7,6 +10,8 @@ public class Pit : MonoBehaviour
     private bool _isRespawning = false;
 
     private CompositeCollider2D _collider;
+
+    private List<Collider2D> _colliders = new List<Collider2D>();
 
     private void Awake()
     {
@@ -34,24 +39,63 @@ public class Pit : MonoBehaviour
 
     private bool IsFullyContained(CompositeCollider2D container, Collider2D target)
     {
-        Bounds containerBounds = container.bounds;
-        Bounds targetBounds = target.bounds;
+        //Bounds containerBounds = container.bounds;
+        //Bounds targetBounds = target.bounds;
 
-        Debug.Log(container.bounds);
-        Debug.Log(target.bounds);
+        //Debug.Log(container.bounds);
+        //Debug.Log(target.bounds);
 
-        Debug.Log("Palla");
+        //Debug.Log("Palla");
 
-        Debug.Log($"{targetBounds.min.x >= containerBounds.min.x}" +
-        $"{targetBounds.max.x <= containerBounds.max.x}" +
-          $"{targetBounds.min.y >= containerBounds.min.y}" +
-            $"{targetBounds.max.y <= containerBounds.max.y}");
+        //Debug.Log($"{targetBounds.min.x >= containerBounds.min.x}" +
+        //$"{targetBounds.max.x <= containerBounds.max.x}" +
+        //  $"{targetBounds.min.y >= containerBounds.min.y}" +
+        //    $"{targetBounds.max.y <= containerBounds.max.y}");
 
-        return targetBounds.min.x > containerBounds.min.x &&
-           targetBounds.max.x < containerBounds.max.x &&
-          targetBounds.min.y > containerBounds.min.y &&
-          targetBounds.max.y > containerBounds.max.y;
+        //return targetBounds.min.x > containerBounds.min.x &&
+        //   targetBounds.max.x < containerBounds.max.x &&
+        //  targetBounds.min.y > containerBounds.min.y &&
+        //  targetBounds.max.y > containerBounds.max.y;
 
+        ContactFilter2D filter = new ContactFilter2D();
+
+        filter.useTriggers = true;
+
+        int count = container.Overlap(filter, _colliders);
+
+        for(int i = 0; i < count; i++)
+        {
+            if (_colliders[i] == target)
+            {
+                return CheckFullContainment(container, target);
+            }
+        }
+        return false;
+
+    }
+
+    private bool CheckFullContainment(CompositeCollider2D container, Collider2D target)
+    {
+        Bounds b = target.bounds;
+
+        Vector2[] points = new Vector2[]
+        {
+            new Vector2(b.min.x, b.min.y),
+            new Vector2(b.max.x, b.min.y),
+            new Vector2(b.min.x,b.max.y),
+            new Vector2(b.max.x,b.max.y),
+            b.center
+        };
+
+        foreach (Vector2 point in points)
+        {
+            Debug.Log($"{!container.OverlapPoint(point)}");
+            if (!container.OverlapPoint(point))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     private IEnumerator RespawnCoroutine(Player player)
