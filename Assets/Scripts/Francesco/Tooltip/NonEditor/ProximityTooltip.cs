26dcc0c606e24dd7916300d5668fae6292350bb5
felipc273 <felipc273@gmail.com>
2026-04-21 19:48:49 +0200

    Completed InteractionTooltip and ProximityTooltip
    + Created tooltip script that lets me Show/Hide a tooltip with a keyText and a text (description)
    + Created proximity tooltip script that only shows the closest InteractionTooltip currently triggering with

---
+++ Assets/Scripts/Francesco/Tooltip/NonEditor/ProximityTooltip.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ProximityTooltip : MonoBehaviour
+{
+    private InteractionTooltip _previousTooltip;
+    private InteractionTooltip _currentTooltip;
+    private List<InteractionTooltip> _detectedTooltips = new();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out InteractionTooltip tooltip))
+        {
+            _detectedTooltips.Add(tooltip);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out InteractionTooltip tooltip))
+        {
+            _previousTooltip = _currentTooltip;
+            _currentTooltip = GetClosestTooltip();
+            if (_currentTooltip != _previousTooltip)
+            {
+                _previousTooltip?.Hide();
+                _currentTooltip?.Show();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out InteractionTooltip tooltip))
+        {
+            if (_currentTooltip && _currentTooltip == tooltip)
+            { 
+                tooltip.Hide();
+            }
+            _detectedTooltips.Remove(tooltip);
+            if (_detectedTooltips.Count == 0)
+            {
+                _currentTooltip = null;
+                _previousTooltip = null;
+            }
+        }
+    }
+
+    private InteractionTooltip GetClosestTooltip()
+    {
+        return _detectedTooltips.OrderBy(ttip => Vector2.Distance(ttip.transform.position, transform.position)).FirstOrDefault();
+    }
+}
