a33bd00fd17ed03bca59a0a0be9e2178b0e86b94
felipc273 <felipc273@gmail.com>
2026-05-11 17:11:48 +0200

    Start ThreeWayRGBSplitter structure

--- Assets/Scripts/Francesco/LightSystem/NonEditor/ThreeWayRGBSplit.cs
+++ Assets/Scripts/Francesco/LightSystem/NonEditor/ThreeWayRGBSplit.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class ThreeWayRGBSplit : MonoBehaviour
@@ -7,4 +8,37 @@ public class ThreeWayRGBSplit : MonoBehaviour
     [SerializeField] private LightEmitter _redEmitter;
     [SerializeField] private LightEmitter _greenEmitter;
     [SerializeField] private LightEmitter _blueEmitter;
+
+    private void OnEnable()
+    {
+        _lightSensor.OnLightActivated.AddListener(LightActivated);
+        _lightSensor.OnLightChanged.AddListener(LightChanged);
+        _lightSensor.OnLightDeactivated.AddListener(LightDeactivated);
+    }
+
+    private void OnDisable()
+    {
+        _lightSensor.OnLightActivated.RemoveListener(LightActivated);
+        _lightSensor.OnLightChanged.RemoveListener(LightChanged);
+        _lightSensor.OnLightDeactivated.RemoveListener(LightDeactivated);
+    }
+
+    private void LightActivated()
+    {
+
+    }
+
+    private void LightDeactivated()
+    {
+    }
+
+    private void LightChanged()
+    {
+
+    }
+
+    private void SetLights()
+    { 
+        
+    }
 }
