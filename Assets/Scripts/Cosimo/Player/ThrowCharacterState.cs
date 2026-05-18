37d5b4f92f7ec2af040b75d02b7bbde454294dca
Noxianblade99 <novateam987@gmail.com>
2026-05-18 17:42:48 +0200

    Upgraded Throwing method with tilemap based method

--- Assets/Scripts/Cosimo/Player/ThrowCharacterState.cs
+++ Assets/Scripts/Cosimo/Player/ThrowCharacterState.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.Scripts.Cosimo.Inventory;
 using System;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem.XR;
 using static UnityEditor.Experimental.GraphView.GraphView;
@@ -9,6 +10,7 @@ internal class ThrowCharacterState : IStateCollision2D
     private Player _owner;
     private PlayerController _ownerController;
     private Animator _animator;
+    private float _timer;
     public ThrowCharacterState(Player player, PlayerController playerController, Animator animator)
     {
         _owner = player;
@@ -45,6 +47,7 @@ internal class ThrowCharacterState : IStateCollision2D
     public void OnStart()
     {
         _ownerController.Rb.linearVelocity = Vector2.zero;
+        _timer = 0f;
         if(InventoryManager.Instance.CanThrowPowder() && IsNearMagicTorch())
         {
             _animator.Play(_owner.ThrowSettings.clipName);
@@ -57,18 +60,29 @@ internal class ThrowCharacterState : IStateCollision2D
         {
             Debug.Log($"Polvere esaurita del colore:{InventoryManager.Instance.SelectedPowder}");
         }
-        _owner.SetState(ECharacterStates.Idle);
+       
     }
 
     private bool IsNearMagicTorch()
     {
-        Vector2 checkPos = (Vector2)_owner.transform.position + _ownerController.LastLookDirection * 0.8f;
-        Collider2D hit = Physics2D.OverlapCircle(checkPos, 0.3f);
-        if (hit != null)
+        Vector3 interactionPos = _owner.transform.position + (Vector3)_ownerController.LastLookDirection * 0.8f;
+
+        Vector3Int cellPos = _owner.PlaceableTilemap.WorldToCell(interactionPos);
+        GameObject itemOnTile = PlacementManager.Instance.GetItemAt(cellPos);
+
+        if (itemOnTile != null)
         {
-            Debug.Log($"[Fisica] Cerchio di controllo ha colpito: {hit.name}");
+            bool hasMagicalTorch = itemOnTile.GetComponentInChildren<MagicalTorch>() != null;
+
+            if (hasMagicalTorch)
+            {
+                Debug.Log($"[Throw] Rilevata una Torcia Magica sulla Tilemap nella cella {cellPos}. Lancio polvere consentito!");
+                return true;
+            }
         }
-        return hit != null && hit.TryGetComponent<MagicalTorch>(out _);
+
+        Debug.LogWarning($"[Throw] Nessuna torcia magica trovata sulla Tilemap nella cella {cellPos}. Lancio annullato.");
+        return false;
     }
 
     public void OnTriggerEnter2D(Collider2D collider)
@@ -88,6 +102,11 @@ internal class ThrowCharacterState : IStateCollision2D
 
     public void OnUpdate()
     {
-        
+        _timer += Time.deltaTime;
+
+        if(_timer>=_owner.ThrowSettings.clip.length)
+        {
+            _owner.SetState(ECharacterStates.Idle);
+        }
     }
 }
