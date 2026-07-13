a488c77da2e1aedf5736d372b810ef95e99c708d
CosimoTatini <novateam987@gmail.com>
2026-07-13 17:06:59 +0200

    Changed things

--- Assets/Scripts/Cosimo/GM/GameManager.cs
+++ Assets/Scripts/Cosimo/GM/GameManager.cs
@@ -1,29 +1,42 @@
-using UnityEngine;
-using DesignPatterns;
 using DesignPatterns.Generics;
-using Unity.VectorGraphics;
-using UnityEngine.SceneManagement;
+using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-   
+
 
     [SerializeField] private UiManager _uiManager;
     [SerializeField] private PlacementManager _placementManager;
     [SerializeField] private InventoryManager _inventoryManager;
     [SerializeField] private LevelManager _levelManager;
     [SerializeField] private GameObject _uiCanvas;
-     
+
     protected override void Awake()
     {
         base.Awake();
-       
+
     }
 
     private void Start()
     {
-        LevelManager.Instance.AddSceneWithLoadingScreen("TutorialCosimo");
+        //LevelManager.Instance.OnLoadingStart += HandleLoadingStart;
+        //LevelManager.Instance.OnLoadingEnd += HandleLoadingEnd;
+        LevelManager.Instance.AddSceneWithLoadingScreen("TutorialFra");
     }
+    //private void HandleLoadingStart()
+    //{
+    //    if (_uiCanvas != null)
+    //    {
+    //        _uiCanvas.SetActive(false);
+    //    }
+    //}
+    //private void HandleLoadingEnd()
+    //{
+    //    if (_uiCanvas != null)
+    //    {
+    //        _uiCanvas.SetActive(true);
+    //    }
+    //}
 
 
 }
