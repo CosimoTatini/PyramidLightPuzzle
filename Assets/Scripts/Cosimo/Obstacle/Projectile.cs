6752d1fca9990b6041fc598a90382b4ba607f004
felipc273 <felipc273@gmail.com>
2026-07-28 12:40:26 +0200

    Projectile Collision fix

--- Assets/Scripts/Cosimo/Obstacle/Projectile.cs
+++ Assets/Scripts/Cosimo/Obstacle/Projectile.cs
@@ -6,33 +6,37 @@ public class Projectile : MonoBehaviour
     private float _lifetime;
     private float _timer;
     private ObjectPooler<Projectile> _pool;
+
+    [SerializeField] private LayerMask _layerMask;
+
     private Tilemap _targetTilemap;
 
     private Rigidbody2D _rb;
+
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
-  
+
     public Rigidbody2D Rb => _rb;
 
     internal void Initialize(ObjectPooler<Projectile> poolRef, Tilemap tilemap, float lifetime)
     {
         _targetTilemap = tilemap;
         _pool = poolRef;
-        _lifetime = lifetime; 
+        _lifetime = lifetime;
         _timer = 0f;
 
-        
+
         _rb.linearVelocity = Vector2.zero;
         _rb.angularVelocity = 0f;
     }
 
     private void Update()
     {
-        
+
         _timer += Time.deltaTime;
         if (_timer >= _lifetime)
         {
@@ -40,17 +44,20 @@ public class Projectile : MonoBehaviour
         }
     }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (_layerMask.Contains(collision.gameObject.layer))
+        {
+            ReturnToPool();
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.TryGetComponent(out Player player))
+        if (collision.gameObject.TryGetComponent(out Player _))
         {
             ReturnToPool();
             return;
-        }
-
-        if (_targetTilemap != null && collision.gameObject == _targetTilemap.gameObject)
-        {
-            ReturnToPool();
         }
     }
 
